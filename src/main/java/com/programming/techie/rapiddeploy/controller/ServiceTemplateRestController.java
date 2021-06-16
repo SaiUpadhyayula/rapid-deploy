2d7dea71a4e7ebd74b7a34a7bfef420e37c0c727
Sai Upadhyayula <subramanyam.sai90@gmail.com>
2021-06-17 00:53:15 +0200

    cleanup: Replaced ResponseEntity annotation with ResponseStatus annottation

--- src/main/java/com/programming/techie/rapiddeploy/controller/ServiceTemplateRestController.java
+++ src/main/java/com/programming/techie/rapiddeploy/controller/ServiceTemplateRestController.java
@@ -4,10 +4,11 @@ import com.programming.techie.rapiddeploy.payload.ServiceTemplateDto;
 import com.programming.techie.rapiddeploy.service.servicetemplate.ServiceTemplateFacade;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("/api/admin/service-template/")
@@ -16,26 +17,28 @@ public class ServiceTemplateRestController {
     private final ServiceTemplateFacade serviceTemplateFacade;
 
     @PostMapping
-    public ResponseEntity<String> create(@RequestBody ServiceTemplateDto serviceTemplateDto) {
+    public String create(@RequestBody ServiceTemplateDto serviceTemplateDto) {
         String guid = serviceTemplateFacade.createServiceTemplate(serviceTemplateDto);
-        return ResponseEntity.status(HttpStatus.OK).body("Service Template created successfully!! GUID - " + guid);
+        return "Service Template created successfully!! GUID - " + guid;
     }
 
     @GetMapping
-    public ResponseEntity<List<ServiceTemplateDto>> getAll() {
-        List<ServiceTemplateDto> serviceTemplates = serviceTemplateFacade.getAllServiceTemplates();
-        return ResponseEntity.status(HttpStatus.OK).body(serviceTemplates);
+    @ResponseStatus(OK)
+    public List<ServiceTemplateDto> getAll() {
+        return serviceTemplateFacade.getAllServiceTemplates();
     }
 
     @PutMapping
-    public ResponseEntity<String> update(@RequestBody ServiceTemplateDto serviceTemplateDto) {
+    @ResponseStatus(OK)
+    public String update(@RequestBody ServiceTemplateDto serviceTemplateDto) {
         serviceTemplateFacade.updateServiceTemplate(serviceTemplateDto);
-        return ResponseEntity.status(HttpStatus.OK).body("Service Template updated successfully!!");
+        return "Service Template updated successfully!!";
     }
 
     @DeleteMapping("/{guid}")
-    public ResponseEntity<String> delete(@PathVariable String guid) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public String delete(@PathVariable String guid) {
         serviceTemplateFacade.deleteServiceTemplate(guid);
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Service Template Deleted Successfully!!");
+        return "Service Template Deleted Successfully!!";
     }
 }
